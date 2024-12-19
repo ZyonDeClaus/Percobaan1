35cb3a2500870d4ef7341660e8896da5710a461a
ZyonDeClaus <wellyainy69@gmail.com>
2024-12-19 11:16:41 +0700

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 07:47:12 2024
+
+@author: HP
+"""
+
+n=("masukkan nilai n:")
+Total=0
+i=1
+for i in range i < 0 (n,i+1)
+    total=0
+    print Total
+    
+r=float("masukkan jari-jari:")
+Luas_Lingkaran = 3.14.r.r
+Print = Luas_Lingkaran
+
+i < 6
+for i in range 1,6:
+    i+1
+    
+for in range 1,11,2:
+    1+(1,11,2)
